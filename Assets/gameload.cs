88dbdd87dd4221187fa0a8c8d7f2280fdf60d252
AveNoF-CZ75 <abelon002@gmail.com>
2017-12-26 20:05:17 +0900

    Unity Practice

---
+++ Assets/gameload.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameload : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Application.LoadLevel("AircraftJet2Axis");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
